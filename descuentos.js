c0b3c0f21638b1fb70e06f7f0c6a250de899c8b5
ssalasm <sebastiansalas@pm.me>
2022-04-26 08:21:26 -0400

    Se crea interacción con HTML para calcular descuento y se muestra en etiqueta <p> dinámica

--- descuentos.js
+++ descuentos.js
@@ -2,12 +2,10 @@
 // const descuento = 18;
 
 function calcularPrecioConDescuento(precio, descuento){
-
     const porcentajePrecioConDescuento = 100 - descuento;
     const precioConDescuento = (precio * porcentajePrecioConDescuento) / 100;
 
     return precioConDescuento;
-
 }
 
 // console.log({
@@ -17,3 +15,17 @@ function calcularPrecioConDescuento(precio, descuento){
 //     precioConDescuento,
 // }
 // );
+
+function onClickButtonPriceDiscount(){
+    const inputPrice = document.getElementById("InputPrice");
+    const inputDiscount = document.getElementById("InputDiscount");
+
+    const priceValue = inputPrice.value;
+    const discountValue = inputDiscount.value;
+
+    const precioConDescuento = calcularPrecioConDescuento(priceValue, discountValue);
+
+    const resultP = document.getElementById("ResultP");
+    resultP.innerText = "El precio con descuento son: $" + precioConDescuento;
+
+}
